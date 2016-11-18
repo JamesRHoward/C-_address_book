78e374f12370c56d51b5b6e7f59b884b18742ceb
James Howard <james@>
2016-11-18 08:13:43 -0800

    created Getters and Setters for contact.cs object. It will have methods for GetAll(), ClearAll(), and Find().

--- Objects/Contact.cs
+++ Objects/Contact.cs
@@ -18,5 +18,33 @@ namespace AddressBook.Objects
       _id = instances.Count;
       _instances.Add(this);
     }
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+    public int GetPhoneNumber()
+    {
+      return _phoneNumber;
+    }
+    public void SetPhoneNumber(int newPhoneNumber)
+    {
+      _phoneNumber = newPhoneNumber;
+    }
+    public string GetEmail()
+    {
+      return _email;
+    }
+    public void SetEmail(string newEmail)
+    {
+      _email = newEmail;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
   }
 }
